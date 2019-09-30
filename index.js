5ec2448ff2ce95a59c9ebe9e220fc1adab3be216
Mark Allen <markallen.webdeveloper@gmail.com>
2019-09-30 11:07:29 +0100

    Functioning API with basic data

--- index.js
+++ index.js
@@ -5,6 +5,18 @@
 const express = require('express')
 const app = express()
 
+const data = {
+  name: 'John Smith',
+  position: 'Manager',
+  location: 'Oxford',
+  telephone: '01865 123456',
+  email: 'jsmith@someaddress.co.uk'
+}
+
+app.get('/', (req, res) => {
+  res.json(data)
+})
+
 app.listen(3000, () => {
   console.log('Simple Node API started - listening on port: 3000')
 })
